3c4578b14cff53f118f14558e18ddc1073082571
Atonalsv <otto_alejandro_marroquin@yahoo.com>
2025-06-07 16:42:04 -0600

    add documents

---
+++ src/main/java/com/retolibros/retolibros/RetolibrosApplication.java
@@ -0,0 +1,13 @@
+package com.retolibros.retolibros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetolibrosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RetolibrosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/retolibros/retolibros/RetolibrosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.retolibros.retolibros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RetolibrosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
